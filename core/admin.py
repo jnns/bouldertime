6fd5e4b64cb4a19bfc3d6cc31a74a4cca8d69876
Jannis Vajen <jvajen@gmail.com>
2020-04-26 12:31:01 +0200

    feat: add models to admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Gym
+from .models import Booking, Gym, PhoneNumber
 
+admin.site.register(Booking)
 admin.site.register(Gym)
+admin.site.register(PhoneNumber)
