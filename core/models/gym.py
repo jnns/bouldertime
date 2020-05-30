caacd19f8f202c38322986d74adbb6de4c1f49a7
Jannis Vajen <jvajen@gmail.com>
2020-05-30 16:52:09 +0200

    feat: add Gym model

--- core/models/gym.py
+++ core/models/gym.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 from datetime import time, timedelta
 
 from django.db import connection, models
+from django.utils.functional import cached_property
 from django.utils.timezone import localdate
 from django.utils.translation import gettext_lazy as _
 
@@ -21,6 +22,7 @@ class Gym(models.Model):
         allow_unicode=True,
         help_text=_("A short name that is used in URLs."),
     )
+    address = models.CharField(verbose_name=_("Address"), blank=True, max_length=255)
     opens_at = models.TimeField(default=at_10am, verbose_name=_("Opens at"))
     closes_at = models.TimeField(default=at_10pm, verbose_name=_("Closes at"))
     max_guests = models.PositiveSmallIntegerField(
@@ -30,45 +32,76 @@ class Gym(models.Model):
             "Also the maximum number of bookings for a time slot."
         ),
     )
+    num_days_available = models.PositiveSmallIntegerField(
+        default=3,
+        verbose_name=_("Bookable days"),
+        help_text=_(
+            "How many days can be booked in advance? "
+            "How many days are shown on the booking overview?"
+        ),
+    )
 
     def __str__(self):
         return self.name
 
-    def get_attendance(self):
+    @cached_property
+    def attendance(self):
         assert self.opens_at.hour
         assert self.closes_at.hour
         assert self.max_guests
         with connection.cursor() as cursor:
             cursor.execute(
                 """
-                with hours as (
-                    select make_time(s.h, 0,0) as hour from generate_series(%s, %s) as s(h)
-                )
-                select date_part('hour', hours.hour),
-                  ceil(count(hour) filter (where (hours.hour, hours.hour) overlaps (b.start, b.end))::numeric / %s * 100)
-                from core_booking b
-                cross join hours
-                group by hours.hour order by hour;
-                """,
-                (self.opens_at.hour, self.closes_at.hour - 1, self.max_guests),
-            )
-            return {
-                int(hour): int(num_bookings) for hour, num_bookings in cursor.fetchall()
-            }
+                with date as (
+                 select date::date from generate_series(
+                  current_date,
+                  current_date + interval '%s days',
+                  interval '1 day'
+                ) as date),
 
-    def is_available(self, hour):
+                hours as (
+                  select make_time(s.h, 0,0) as hour from generate_series(%s, %s) as s(h)
+                )
+
+                select
+                  d.date,
+                  date_part('hour', hours.hour) as hour,
+                  ceil(
+                    count(hour) filter (
+                      where b.status in ('NEW', 'CONFIRMED')
+                      and (d.date + hours.hour, d.date + hours.hour) overlaps (b.date + b.start, b.date + b.end)
+                    )::numeric / %s * 100
+                  ) as booking_count
+                from hours
+                left outer join core_booking b on (1=1)
+                left outer join date d on (1=1)
+                group by d.date, hour order by d.date, hour;
+                """,
+                (
+                    self.num_days_available - 1,
+                    self.opens_at.hour,
+                    self.closes_at.hour - 1,
+                    self.max_guests,
+                ),
+            )
+            attendance = defaultdict(dict)
+            for date, hour, num_bookings in cursor.fetchall():
+                attendance[str(date)][int(hour)] = int(num_bookings)
+            return attendance
+
+    def is_available(self, date, hour):
         try:
-            return self.get_attendance()[hour] < 100
+            return self.attendance[str(date)][hour] < 100
         except KeyError:
             return False
 
-    def get_available_end_hours(self, hour):
+    def get_available_end_hours(self, date, hour):
         end_hours = []
         max_bookable_hours = 3
         for i in range(max_bookable_hours):
             checked_hour = hour + i
             end_hour = checked_hour + 1
-            if self.is_available(checked_hour):
+            if self.is_available(date, checked_hour):
                 end_hours.append(end_hour)
             else:
                 break
--- core/tests/models/test_gym.py
+++ core/tests/models/test_gym.py
@@ -1,9 +1,10 @@
-from datetime import time
-from unittest.mock import patch
+from datetime import time, timedelta
+from unittest.mock import PropertyMock, patch
 
 import pytest
+from django.utils.timezone import localdate
 
-from core.models import Booking, Gym
+from core.models import Gym
 
 
 @pytest.mark.parametrize(
@@ -19,38 +20,90 @@ def test_get_opening_hours(opens_at, closes_at, expected):
     assert list(gym.get_opening_hours()) == list(expected)
 
 
-@patch("core.models.Gym.get_attendance")
-def test_get_bookables(mocked_get_attendance):
-    mocked_get_attendance.return_value = {
-        8: 100,
-        9: 0,
-        10: 0,
-        11: 0,
-        12: 100,
-        13: 0,
-        14: 0,
-        15: 100,
-        16: 0,
+@patch("core.models.Gym.attendance", new_callable=PropertyMock)
+def test_get_bookables(mocked_attendance):
+    mocked_attendance.return_value = {
+        str(localdate()): {
+            8: 100,
+            9: 0,
+            10: 0,
+            11: 0,
+            12: 100,
+            13: 0,
+            14: 0,
+            15: 100,
+            16: 0,
+        }
     }
-    bookables = Gym(opens_at=time(8), closes_at=time(17)).get_bookables()
-    assert bookables == {
-        8: [],
-        9: [10, 11, 12],
-        10: [11, 12],
-        11: [12],
-        12: [],
-        13: [14, 15],
-        14: [15],
-        15: [],
-        16: [17],
+    gym = Gym(opens_at=time(8), closes_at=time(17))
+    assert gym.get_bookables() == {
+        str(localdate()): {
+            8: [],
+            9: [10, 11, 12],
+            10: [11, 12],
+            11: [12],
+            12: [],
+            13: [14, 15],
+            14: [15],
+            15: [],
+            16: [17],
+        },
+        str(localdate() + timedelta(days=1)): {
+            8: [],
+            9: [],
+            10: [],
+            11: [],
+            12: [],
+            13: [],
+            14: [],
+            15: [],
+            16: [],
+        },
+        str(localdate() + timedelta(days=2)): {
+            8: [],
+            9: [],
+            10: [],
+            11: [],
+            12: [],
+            13: [],
+            14: [],
+            15: [],
+            16: [],
+        },
     }
 
 
 @pytest.mark.django_db
-def test_get_attendance():
+def test_attendance(booking_factory):
+    booking = booking_factory(
+        start="10:00",
+        end="11:00",
+        phone_number="+49123456789",
+        gym__max_guests=4,
+        gym__opens_at=time(10),
+        gym__closes_at=time(12),
+    )
+    booking_factory(
+        start="10:00",
+        end="11:00",
+        phone_number="+49123456789",
+        gym=booking.gym,
+        status=booking.STATUS_CANCELLED,
+    )
+    today = localdate()
+    assert booking.gym.attendance == {
+        str(today): {10: 25, 11: 0},
+        str(today + timedelta(days=1)): {10: 0, 11: 0},
+        str(today + timedelta(days=2)): {10: 0, 11: 0},
+    }
+
+
+@pytest.mark.django_db
+def test_attendance_without_bookings():
     gym = Gym.objects.create(opens_at=time(10), closes_at=time(12), max_guests=4)
-    Booking.objects.create(gym=gym, start=time(10), end=time(11))
-    assert gym.get_attendance() == {
-        10: 25,
-        11: 0,
+    today = localdate()
+    assert gym.attendance == {
+        str(today): {10: 0, 11: 0},
+        str(today + timedelta(days=1)): {10: 0, 11: 0},
+        str(today + timedelta(days=2)): {10: 0, 11: 0},
     }
