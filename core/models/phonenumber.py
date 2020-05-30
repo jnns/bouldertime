4b7c249756fde15f0390801d1cfab0d493c9ef83
Jannis Vajen <jvajen@gmail.com>
2020-05-30 16:54:20 +0200

    feat: add PhoneNumber model

--- core/models/phonenumber.py
+++ core/models/phonenumber.py
@@ -1,4 +1,3 @@
-import re
 from hashlib import sha1
 
 from django.db import models
@@ -6,32 +5,49 @@ from django.utils.timezone import now
 
 
 class PhoneNumberManager(models.Manager):
-    def create(self, phone_number):
-        assert phone_number
-        phone_number = PhoneNumber.from_number(phone_number)
-        return super().create(hash=sha1(phone_number.encode("utf-8")).hexdigest())
+    def create(self, plain_number, *args, **kwargs):
+        kwargs["hashed_number"] = PhoneNumber(plain_number=plain_number).hashed_number
+        return super().create(*args, **kwargs)
+
+    def get_or_create(self, defaults=None, **kwargs):
+        if "plain_number" in kwargs:
+            kwargs["hashed_number"] = PhoneNumber(
+                plain_number=kwargs.pop("plain_number")
+            ).hashed_number
+        return super().get_or_create(defaults, **kwargs)
+
+    def blocked(self):
+        return self.get_queryset().filter(blocked_due_to__isnull=False)
 
 
 class PhoneNumber(models.Model):
-    hash = models.CharField(unique=True, max_length=255)
+    hashed_number = models.CharField(unique=True, default=None, max_length=255)
     verified_at = models.DateTimeField(null=True)
     blocked_due_to = models.ForeignKey("Booking", on_delete=models.CASCADE, null=True)
 
     objects = PhoneNumberManager()
 
-    @staticmethod
-    def from_number(value):
-        value = value.replace("/", "").replace(" ", "")
-        value = re.sub(r"^0049", "+49", value)
-        value = re.sub(r"^0", "+49", value)
-        return value
-
     def __str__(self):
-        return self.hash
+        return self.hashed_number or "None"
+
+    def __init__(self, *args, **kwargs):
+        plain_number = kwargs.pop("plain_number", None)
+        super().__init__(*args, **kwargs)
+        if plain_number:
+            self.hashed_number = sha1(plain_number.encode("utf-8")).hexdigest()
 
     def verify(self):
+        assert self.hashed_number
         self.verified_at = now()
         if self.id:
             self.save(update_fields=["verified_at"])
         else:
             self.save()
+
+    @property
+    def is_blocked(self):
+        return bool(self.blocked_due_to)
+
+    def block(self, booking):
+        self.blocked_due_to = booking
+        self.save(update_fields=["blocked_due_to"])
--- core/tests/models/test_phonenumber.py
+++ core/tests/models/test_phonenumber.py
@@ -8,41 +8,22 @@ from core.models import PhoneNumber
 pytestmark = pytest.mark.django_db
 
 
-@pytest.mark.parametrize(
-    "real_number", ["01234567890", "0123/4567890", "0123 4567890", "0123 / 4567890"],
-)
-def test_create_from_real_number(real_number):
+def test_init():
+    assert str(PhoneNumber())
+    assert (
+        str(PhoneNumber(plain_number="+491234567890"))
+        == "9bd6ad2d480f48e483d87fb7bf456c2ad0dc3744"
+    )
+
+
+def test_hashed_number():
     hashed_number = hashlib.sha1(b"+491234567890").hexdigest()
-    assert str(PhoneNumber.objects.create(real_number)) == hashed_number
-
-
-@pytest.mark.parametrize(
-    "input,expected",
-    [
-        ("01234567890", "+491234567890"),
-        ("0123 4567890", "+491234567890"),
-        ("0123/4567890", "+491234567890"),
-        ("0123 / 4567890", "+491234567890"),
-        ("00491234567890", "+491234567890"),
-        ("0049 123 4567890", "+491234567890"),
-        ("0049123/4567890", "+491234567890"),
-        ("0049 123 / 4567890", "+491234567890"),
-        ("+491234567890", "+491234567890"),
-        ("+49 1234567890", "+491234567890"),
-        ("+49123 4567890", "+491234567890"),
-        ("+49 123/4567890", "+491234567890"),
-        ("+49123 / 4567890", "+491234567890"),
-        ("+49123 / 4567890", "+491234567890"),
-        ("+49123 / 0049 0049", "+4912300490049"),
-    ],
-)
-def test_from_number(input, expected):
-    assert PhoneNumber.from_number(input) == expected
+    assert str(PhoneNumber.objects.create("+491234567890")) == hashed_number
 
 
 def test_verify():
     pre = now()
-    phone_no = PhoneNumber()
-    phone_no.verify()
+    phone_number = PhoneNumber(plain_number="01234567890")
+    phone_number.verify()
     post = now()
-    assert pre < phone_no.verified_at < post
+    assert pre < phone_number.verified_at < post
